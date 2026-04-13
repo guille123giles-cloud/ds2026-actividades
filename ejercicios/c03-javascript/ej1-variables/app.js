aecc75c2a4de0976323c560716512d2eeef82484
Guillermo Giles <guille123giles@gmail.com>
2026-04-13 14:30:11 -0300

    Completa Ejercicio 1: Variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,16 @@
+// Declarar con const: tu nombre, tu edad, la materia que estás cursando
+const nombre = "Guillermo";
+const edad = 21; 
+const materia = "Desarrollo de Software";
+
+// Mostrar en consola un mensaje con template literals
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+// Declarar con let un contador en 0. Sumarle 1 tres veces.
+let contador = 0;
+contador += 1;
+contador += 1;
+contador += 1;
+
+// Mostrar el valor final.
+console.log("Valor final del contador:", contador);
